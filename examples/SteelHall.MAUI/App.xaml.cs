b6d86174b478e69cde6d8aff731433c2ba675dfb
Rebecca <rebeccagoebel@gmx.de>
2023-06-22 17:12:35 +0200

    Change UI

--- examples/SteelHall.MAUI/App.xaml.cs
+++ examples/SteelHall.MAUI/App.xaml.cs
@@ -1,10 +1,31 @@
-﻿namespace SteelHall.MAUI
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+namespace SteelHall.MAUI
 {
     public partial class App : Application
     {
+        const int WindowWidth = 900;
+        const int WindowHeight = 700;
         public App()
         {
             InitializeComponent();
+
+            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            {
+#if WINDOWS
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+#endif
+            });
 
             MainPage = new AppShell();
         }
